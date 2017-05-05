add1dc8c9ba9e211746d53aafc2dfc06307908a0
hazratgs <hazratgs@gmail.com>
2017-05-05 10:41:04 +0300

    Перенес запуск с 33 миныты на 0

--- bin/cron.js
+++ bin/cron.js
@@ -9,7 +9,7 @@ const Instanode = require('./instanode');
 let activeTask = [];
 
 // Запускаем активные задания
-cron.schedule('33 */1 * * *', () => {
+cron.schedule('00 */1 * * *', () => {
     Task.currentList()
         .then(tasks => {
             console.log('-----------')
