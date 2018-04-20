3d543319093668db5e4e8c29693ef25c0bba3342
hazratgs <hazratgs@gmail.com>
2018-04-20 12:32:10 +0300

    Added webhook on express app

--- telegram.js
+++ telegram.js
@@ -1,5 +1,26 @@
 const TelegramBot = require('node-telegram-bot-api')
 const conf = require('./conf')
-const bot = new TelegramBot(conf.token, { polling: true })
+const bot = new TelegramBot(conf.token)
+const express = require('express')
+const bodyParser = require('body-parser')
+
+// This informs the Telegram servers of the new webhook.
+bot.setWebHook(`${conf.url}/bot${conf.token}`)
+
+const app = express()
+
+// parse the updates to JSON
+app.use(bodyParser.json())
+
+// We are receiving updates at the route below!
+app.post(`/bot${conf.token}`, (req, res) => {
+  bot.processUpdate(req.body)
+  res.sendStatus(200)
+})
+
+// Start Express Server
+app.listen(conf.port, () => {
+  console.log(`Express server is listening on ${conf.port}`)
+})
 
 module.exports = bot
