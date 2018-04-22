611a2e5508fe547601636e35f509898915c57ff1
hazratgs <hazratgs@gmail.com>
2018-04-22 15:16:14 +0300

    pooling for dev

--- telegram.js
+++ telegram.js
@@ -1,26 +1,35 @@
 const TelegramBot = require('node-telegram-bot-api')
 const conf = require('./conf')
-const bot = new TelegramBot(conf.token)
 const express = require('express')
 const bodyParser = require('body-parser')
 
-// This informs the Telegram servers of the new webhook.
-bot.setWebHook(`${conf.url}/bot${conf.token}`)
+let bot = null
 
-const app = express()
+if (process.env.DEV) {
+  // initial pooling bot
+  bot = new TelegramBot(conf.token, { polling: true })
+} else {
+  // initial webhook
+  bot = new TelegramBot(conf.token)
 
-// parse the updates to JSON
-app.use(bodyParser.json())
+  // This informs the Telegram servers of the new webhook.
+  bot.setWebHook(`${conf.url}/bot${conf.token}`)
 
-// We are receiving updates at the route below!
-app.post(`/bot${conf.token}`, (req, res) => {
-  bot.processUpdate(req.body)
-  res.sendStatus(200)
-})
+  const app = express()
 
-// Start Express Server
-app.listen(conf.port, () => {
-  console.log(`Express server is listening on ${conf.port}`)
-})
+  // parse the updates to JSON
+  app.use(bodyParser.json())
+
+  // We are receiving updates at the route below!
+  app.post(`/bot${conf.token}`, (req, res) => {
+    bot.processUpdate(req.body)
+    res.sendStatus(200)
+  })
+
+  // Start Express Server
+  app.listen(conf.port, () => {
+    console.log(`Express server is listening on ${conf.port}`)
+  })
+}
 
 module.exports = bot
