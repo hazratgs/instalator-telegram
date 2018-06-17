9d81b2e6f645709589c277714c76d56d5568538c
hazratgs <hazratgs@gmail.com>
2018-06-17 16:31:15 +0300

    add default start minutes

--- app/models/task.js
+++ app/models/task.js
@@ -8,7 +8,7 @@ const TaskSchema = new db.mongoose.Schema({
   params: { type: Object, required: [true, 'paramsRequired'] }, // Array for storage specific to the data type
   status: { type: String, default: 'active' }, // Job status (active, cancel, success)
   date: { type: Date, default: Date.now },
-  start: { type: Number, required: [true, 'startReqiured'] } // minute start
+  start: { type: Number, default: new Date().getMinutes(), required: [true, 'startReqiured'] } // minute start
 })
 
 exports.Task = db.connect.model('Task', TaskSchema)
